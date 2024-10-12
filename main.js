73f7bf9645251eb20dbb1f06d389bc7e6d7f7d6e
julian <juliansolo7189@gmail.com>
2024-10-12 11:08:46 -0500

    negacion en js

---
+++ main.js
@@ -0,0 +1,8 @@
+const negacion = !true; 
+// Se declara una constante llamada 'negacion' y se le asigna el valor opuesto (negado) de 'true'.
+// El operador '!' es el operador lógico de negación, por lo que '!true' devuelve 'false'.
+// Entonces, 'negacion' tendrá el valor 'false'.
+
+console.log(negacion);   
+// Se utiliza 'console.log()' para imprimir el valor de la constante 'negacion' en la consola.
+// En este caso, se imprimirá 'false' porque es el valor que se almacenó en 'negacion'.
